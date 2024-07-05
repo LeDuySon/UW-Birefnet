7f7d1f4eba07b5670ebdf3efdaf01dbdc8c24d68
EC2 Default User <ec2-user@ip-10-1-0-42.ap-southeast-2.compute.internal>
2024-07-05 05:31:41 +0000

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='1.0',
+    packages=find_packages(),
+    author='Your Name',
+    author_email='your_email@example.com',
+    description='A brief description of your package',
+    url='https://github.com/your_username/your_repository',
+)
